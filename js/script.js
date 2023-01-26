7a26542da8c2c7c1124beb10c72eae694b49e1bc
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-26 18:02:56 +0200

    ver 2.0.1

--- js/script.js
+++ js/script.js
@@ -18,87 +18,85 @@ let player2 = {
 let inGame = true;
 let winScore = 100;
 
-while (inGame) {
-    rollDice.addEventListener(`click`, function (e) {
-        const drop1 = Math.floor(Math.random() * 6 + 1);
-        const drop2 = Math.floor(Math.random() * 6 + 1);
-        switch (drop1) {
-            case 1:
-                cube1.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
-                break;
-            case 2:
-                cube1.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
-                break;
-            case 3:
-                cube1.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
-                break;
-            case 4:
-                cube1.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
-                break;
-            case 5:
-                cube1.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
-                break;
-            case 6:
-                cube1.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
-                break;
-        }
-        switch (drop2) {
-            case 1:
-                cube2.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
-                break;
-            case 2:
-                cube2.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
-                break;
-            case 3:
-                cube2.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
-                break;
-            case 4:
-                cube2.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
-                break;
-            case 5:
-                cube2.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
-                break;
-            case 6:
-                cube2.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
-                break;
-        }
-        if (whoPlay === 1) {
-            player1.currentScore += drop1;
-            player1.currentScore += drop2;
-            playerOneCurrent.textContent = `${player1.currentScore}`;
-        } else {
-            player2.currentScore += drop1;
-            player2.currentScore += drop2;
-            playerTwoCurrent.textContent = `${player2.currentScore}`;
-        }
-    });
+rollDice.addEventListener(`click`, function (e) {
+    const drop1 = Math.floor(Math.random() * 6 + 1);
+    const drop2 = Math.floor(Math.random() * 6 + 1);
+    switch (drop1) {
+        case 1:
+            cube1.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
+            break;
+        case 2:
+            cube1.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
+            break;
+        case 3:
+            cube1.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
+            break;
+        case 4:
+            cube1.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
+            break;
+        case 5:
+            cube1.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
+            break;
+        case 6:
+            cube1.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
+            break;
+    }
+    switch (drop2) {
+        case 1:
+            cube2.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
+            break;
+        case 2:
+            cube2.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
+            break;
+        case 3:
+            cube2.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
+            break;
+        case 4:
+            cube2.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
+            break;
+        case 5:
+            cube2.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
+            break;
+        case 6:
+            cube2.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
+            break;
+    }
+    if (whoPlay === 1) {
+        player1.currentScore += drop1;
+        player1.currentScore += drop2;
+        playerOneCurrent.textContent = `${player1.currentScore}`;
+    } else {
+        player2.currentScore += drop1;
+        player2.currentScore += drop2;
+        playerTwoCurrent.textContent = `${player2.currentScore}`;
+    }
+});
 
-    hold.addEventListener(`click`, function (e) {
-        if (whoPlay === 1) {
-            player1.score += player1.currentScore;
-            player1.currentScore = 0;
-            playerOneScore.textContent = `${player1.score}`;
-            playerOneCurrent.textContent = `0`;
-            if (player1.score === winScore) {
-                alert(`PLAYER 1 is the WINNER!!!!`);
-                inGame = false;
-            } else if (player1.score > winScore) {
-                alert(`PLAYER 2 is the WINNER`);
-                inGame = false;
-            }
-        } else {
-            player2.score += player2.currentScore;
-            player2.currentScore = 0;
-            playerTwoScore.textContent = `${player2.score}`;
-            playerTwoCurrent.textContent = `0`;
-            if (player2.score === winScore) {
-                alert(`PLAYER 2 is the WINNER!!!!`);
-                inGame = false;
-            } else if (player2.score > winScore) {
-                alert(`PLAYER 1 is the WINNER`);
-                inGame = false;
-            }
+hold.addEventListener(`click`, function (e) {
+    if (whoPlay === 1) {
+        player1.score += player1.currentScore;
+        player1.currentScore = 0;
+        playerOneScore.textContent = `${player1.score}`;
+        playerOneCurrent.textContent = `0`;
+        if (player1.score === winScore) {
+            alert(`PLAYER 1 is the WINNER!!!!`);
+            inGame = false;
+        } else if (player1.score > winScore) {
+            alert(`PLAYER 2 is the WINNER`);
+            inGame = false;
         }
-        whoPlay = whoPlay === 1 ? 2 : 1;
-    });
-}
+    } else {
+        player2.score += player2.currentScore;
+        player2.currentScore = 0;
+        playerTwoScore.textContent = `${player2.score}`;
+        playerTwoCurrent.textContent = `0`;
+        if (player2.score === winScore) {
+            alert(`PLAYER 2 is the WINNER!!!!`);
+            inGame = false;
+        } else if (player2.score > winScore) {
+            alert(`PLAYER 1 is the WINNER`);
+            inGame = false;
+        }
+    }
+    whoPlay = whoPlay === 1 ? 2 : 1;
+});
