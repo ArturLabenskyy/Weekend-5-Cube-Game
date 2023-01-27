884cc4b883d5a590c426641134ddc35427c36d16
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-27 15:34:30 +0200

    css added

--- js/script.js
+++ js/script.js
@@ -102,42 +102,54 @@ function rollClick(player) {
     const drop2 = Math.floor(Math.random() * 6 + 1);
     switch (drop1) {
         case 1:
-            cube1.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
+            cube1.src = `../assets/images/dice1.png`;
+            // cube1.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
             break;
         case 2:
-            cube1.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
+            cube1.src = `../assets/images/dice2.png`;
+            // cube1.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
             break;
         case 3:
-            cube1.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
+            cube1.src = `../assets/images/dice3.png`;
+            // cube1.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
             break;
         case 4:
-            cube1.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
+            cube1.src = `../assets/images/dice4.png`;
+            // cube1.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
             break;
         case 5:
-            cube1.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
+            cube1.src = `../assets/images/dice5.png`;
+            // cube1.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
             break;
         case 6:
-            cube1.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
+            cube1.src = `../assets/images/dice6.png`;
+            // cube1.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
             break;
     }
     switch (drop2) {
         case 1:
-            cube2.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
+            cube2.src = `../assets/images/dice1.png`;
+            // cube2.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
             break;
         case 2:
-            cube2.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
+            cube2.src = `../assets/images/dice2.png`;
+            // cube2.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
             break;
         case 3:
-            cube2.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
+            cube2.src = `../assets/images/dice3.png`;
+            // cube2.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
             break;
         case 4:
-            cube2.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
+            cube2.src = `../assets/images/dice4.png`;
+            // cube2.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
             break;
         case 5:
-            cube2.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
+            cube2.src = `../assets/images/dice5.png`;
+            // cube2.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
             break;
         case 6:
-            cube2.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
+            cube2.src = `../assets/images/dice6.png`;
+            // cube2.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
             break;
     }
     if (drop1 === 6 && drop2 === 6) {
