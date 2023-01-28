159f7c4f8a3330e5aac913743f308037c522e3a0
ArturLabenskyy <dodik.dod@hotmail.com>
2023-01-28 15:13:27 +0200

    final

--- js/script.js
+++ js/script.js
@@ -13,12 +13,21 @@ const newGame = document.querySelector(`.new-game`);
 const displays = document.querySelectorAll(`.player-box`);
 const hidden1 = document.querySelector(`.hidden1`);
 const hidden2 = document.querySelector(`.hidden2`);
-const audio = new Audio("../assets/audio/dice.mp3");
+const rollSound = new Audio(`../assets/audio/dice.mp3`);
+const winSound = new Audio(`../assets/audio/win.mp3`);
 const double6 = document.querySelector(`.double-6`);
 const topBorder1 = document.querySelector(`.p1`);
 const topBorder2 = document.querySelector(`.p2`);
 const botBorder1 = document.querySelector(`#current1`);
 const botBorder2 = document.querySelector(`#current2`);
+const imgArr = [
+    `../assets/images/dice1.png`,
+    `../assets/images/dice2.png`,
+    `../assets/images/dice3.png`,
+    `../assets/images/dice4.png`,
+    `../assets/images/dice5.png`,
+    `../assets/images/dice6.png`,
+];
 
 let whoPlay = 1;
 let player1 = {
@@ -102,61 +111,11 @@ function holdClick(player) {
 }
 
 function rollClick(player) {
-    audio.play();
+    rollSound.play();
     const drop1 = Math.floor(Math.random() * 6 + 1);
     const drop2 = Math.floor(Math.random() * 6 + 1);
-    switch (drop1) {
-        case 1:
-            cube1.src = `../assets/images/dice1.png`;
-            // cube1.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
-            break;
-        case 2:
-            cube1.src = `../assets/images/dice2.png`;
-            // cube1.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
-            break;
-        case 3:
-            cube1.src = `../assets/images/dice3.png`;
-            // cube1.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
-            break;
-        case 4:
-            cube1.src = `../assets/images/dice4.png`;
-            // cube1.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
-            break;
-        case 5:
-            cube1.src = `../assets/images/dice5.png`;
-            // cube1.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
-            break;
-        case 6:
-            cube1.src = `../assets/images/dice6.png`;
-            // cube1.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
-            break;
-    }
-    switch (drop2) {
-        case 1:
-            cube2.src = `../assets/images/dice1.png`;
-            // cube2.style.background = `url(../assets/images/dice1.png) no-repeat center center/cover`;
-            break;
-        case 2:
-            cube2.src = `../assets/images/dice2.png`;
-            // cube2.style.background = `url(../assets/images/dice2.png) no-repeat center center/cover`;
-            break;
-        case 3:
-            cube2.src = `../assets/images/dice3.png`;
-            // cube2.style.background = `url(../assets/images/dice3.png) no-repeat center center/cover`;
-            break;
-        case 4:
-            cube2.src = `../assets/images/dice4.png`;
-            // cube2.style.background = `url(../assets/images/dice4.png) no-repeat center center/cover`;
-            break;
-        case 5:
-            cube2.src = `../assets/images/dice5.png`;
-            // cube2.style.background = `url(../assets/images/dice5.png) no-repeat center center/cover`;
-            break;
-        case 6:
-            cube2.src = `../assets/images/dice6.png`;
-            // cube2.style.background = `url(../assets/images/dice6.png) no-repeat center center/cover`;
-            break;
-    }
+    cube1.src = imgArr[drop1 - 1];
+    cube2.src = imgArr[drop2 - 1];
     if (drop1 === 6 && drop2 === 6) {
         double6.style.display = "block";
         setTimeout(() => {
@@ -211,6 +170,7 @@ function changePlayer(player) {
 }
 
 function winScreen(player) {
+    winSound.play();
     if (player === 1) {
         hidden1.classList.add("win-animation");
         if (player1.score === winScore) {
